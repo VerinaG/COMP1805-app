da0695ce8395db2e4993a217534f84d3f57cf2fd
Verina Georgy <verina.georgy@hotmail.com>
2019-07-16 23:51:58 -0400

    Split Screen

--- COMP1805App/www/scripts/index.js
+++ COMP1805App/www/scripts/index.js
@@ -5,33 +5,35 @@
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+
+        /*
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
+        */
 
         //Listen for PDF Viewer button click event
-        document.getElementById("singleView").addEventListener("click", viewSinglePdf);
-        //var singleViewButton = document.querySelector(#singleView);
-        //singleViewButton.addEventListener("click", viewSinglePdf, false");
-        pdfjsLib.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.js';
+        document.getElementById("singleView").addEventListener("click", viewSinglePdf);        
+        document.getElementById("leftView").addEventListener("click", viewLeftPdf);
+        document.getElementById("rightView").addEventListener("click", viewRightPdf);
 
+        pdfjsLib.GlobalWorkerOptions.workerSrc = '../../node_modules/pdfjs-dist/build/pdf.worker.js';
     };
 
     
     //View single PDF
     function viewSinglePdf() {
 
-        //Waiting for file to be chosen
+        //Waiting for file to be chosen in browse dialog box
         document.getElementById("file-input").onchange = function (event) {
 
             var file = event.target.files[0];
@@ -57,6 +59,51 @@
                     }
                 });
 
+                //Render page
+                function renderPage(pageNumber, canvas) {
+                    thePDF.getPage(pageNumber).then(function (page) {
+                        var viewport = page.getViewport(scale);
+                        canvas.height = viewport.height;
+                        canvas.width = viewport.width;
+                        page.render({ canvasContext: canvas.getContext('2d'), viewport: viewport });
+                    });
+                };
+            };
+
+            fileReader.readAsArrayBuffer(file);
+        }  
+    };
+
+    //View left PDF
+    function viewLeftPdf() {
+
+        //Waiting for file to be chosen in browse dialog box
+        document.getElementById("file-input-left").onchange = function (event) {
+
+            var file = event.target.files[0];
+
+            var fileReader = new FileReader();
+
+            fileReader.onload = function () {
+                var typedarray = new Uint8Array(this.result);
+
+                var thePDF = null;
+                var scale = 1;
+
+                //Displaying PDF
+                pdfjsLib.getDocument(typedarray).promise.then(function (pdf) {
+                    thePDF = pdf;
+                    var viewer = document.getElementById('pdf-left');
+
+                    for (var page = 1; page <= pdf.numPages; page++) {
+                        var canvas = document.createElement("canvas");
+                        canvas.className = 'pdf-page-canvas';
+                        viewer.appendChild(canvas);
+                        renderPage(page, canvas);
+                    }
+                });
+
+                //Render page
                 function renderPage(pageNumber, canvas) {
                     thePDF.getPage(pageNumber).then(function (page) {
                         var viewport = page.getViewport(scale);
@@ -69,10 +116,51 @@
 
             fileReader.readAsArrayBuffer(file);
         }
-
-        
     };
 
+    //View right PDF
+    function viewRightPdf() {
+
+        //Waiting for file to be chosen in browse dialog box
+        document.getElementById("file-input-right").onchange = function (event) {
+
+            var file = event.target.files[0];
+
+            var fileReader = new FileReader();
+
+            fileReader.onload = function () {
+                var typedarray = new Uint8Array(this.result);
+
+                var thePDF = null;
+                var scale = 1;
+
+                //Displaying PDF
+                pdfjsLib.getDocument(typedarray).promise.then(function (pdf) {
+                    thePDF = pdf;
+                    var viewer = document.getElementById('pdf-right');
+
+                    for (var page = 1; page <= pdf.numPages; page++) {
+                        var canvas = document.createElement("canvas");
+                        canvas.className = 'pdf-page-canvas';
+                        viewer.appendChild(canvas);
+                        renderPage(page, canvas);
+                    }
+                });
+
+                //Render page
+                function renderPage(pageNumber, canvas) {
+                    thePDF.getPage(pageNumber).then(function (page) {
+                        var viewport = page.getViewport(scale);
+                        canvas.height = viewport.height;
+                        canvas.width = viewport.width;
+                        page.render({ canvasContext: canvas.getContext('2d'), viewport: viewport });
+                    });
+                };
+            };
+
+            fileReader.readAsArrayBuffer(file);
+        }
+    };
 
     function onPause() {
         // TODO: This application has been suspended. Save application state here.
